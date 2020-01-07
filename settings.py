bab356bc5e9ea4b81aee8fcb773b9ba868b8976b
Fabian P. Schmidt <kerel@mailbox.org>
2020-01-07 17:35:43 +0100

    Fix type-casting in settings
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- settings.py
+++ settings.py
@@ -4,9 +4,9 @@ from decouple import config
 DB_BASE_URL = config('DB_BASE_URL', default='https://db.satnogs.org')
 NETWORK_BASE_URL = config('NETWORK_BASE_URL', default='https://network.satnogs.org')
 CACHE_DIR = config('CACHE_DIR', default='/tmp/cache')
-CACHE_AGE = config('CACHE_AGE', default=24)  # In hours
-MAX_NORAD_CAT_ID = config('MAX_NORAD_CAT_ID', default=90000)
-MIN_PASS_DURATION = config('MIN_PASS_DURATION', default=3)  # In minutes
+CACHE_AGE = config('CACHE_AGE', default=24, cast=float)  # In hours
+MAX_NORAD_CAT_ID = config('MAX_NORAD_CAT_ID', default=90000, cast=int)
+MIN_PASS_DURATION = config('MIN_PASS_DURATION', default=3, cast=float)  # In minutes
 
 # Credentials
 SATNOGS_API_TOKEN = config('SATNOGS_API_TOKEN', default='')
