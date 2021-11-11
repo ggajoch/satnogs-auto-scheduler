82a675e9ab2f4d24264862eda86c0680714bfc4a
Fabian P. Schmidt <kerel@mailbox.org>
2021-11-11 20:53:28 +0100

    auto_scheduler_setup: Add CLI argument parser
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- auto_scheduler_setup.py
+++ auto_scheduler_setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+import argparse
 import logging
 import sys
 
@@ -8,9 +9,6 @@ from auto_scheduler.io import read_tles, read_transmitters
 from auto_scheduler.satnogs_client import get_groundstation_info
 from auto_scheduler.utils import satellites_from_transmitters
 
-STATION_ID = <YOUR_STATION_ID>
-PRIO_PREF_FILE = "prio_pref_sats.txt"
-PRIO_FILE = "priorities_{}.txt".format(STATION_ID)
 
 class Preference:
     """Preference class"""
@@ -30,14 +28,13 @@ class Preference:
                     and (self.search_term in sat.mode or self.search_term in sat.name))
 
 
-def main():
-
+def main(station_id, constrains_file, priorities_file):
     logging.basicConfig(level=logging.INFO,
                         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
     logging.info("Reading file with prefs")
     prefered_priorities = []
-    with open(PRIO_PREF_FILE, "r") as prio_file:
+    with open(constrains_file, "r") as prio_file:
         for i in prio_file.readlines():
             # in case empty lines or comments
             if len(i) <= 1 or i[0] in ("#", "", " "):
@@ -50,8 +47,8 @@ def main():
                 logging.error(err)
                 pass
 
-    ground_station = get_groundstation_info(STATION_ID, allow_testing=True)
-    cache = CacheManager(STATION_ID, ground_station['antenna'], settings.CACHE_DIR,
+    ground_station = get_groundstation_info(station_id, allow_testing=True)
+    cache = CacheManager(station_id, ground_station['antenna'], settings.CACHE_DIR,
                          settings.CACHE_AGE, settings.MAX_NORAD_CAT_ID)
     logging.info("Last cache update: {}".format(cache.last_update()))
     logging.info("Cache update needed: {}".format(cache.update_needed()))
@@ -80,7 +77,7 @@ def main():
                     sat_list[sat.id] = (sat.id, prio.priority, sat.transmitter)
 
     # save the priorities in the file
-    with open(PRIO_FILE, "w") as out_file:
+    with open(priorities_file, "w") as out_file:
         for elem in sat_list.values():
             out_file.writelines("{} {} {}\n".format(elem[0], elem[1], elem[2]))
 
@@ -88,4 +85,16 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(
+        description="Generate a priority list from a set of constraints given in a file.")
+    parser.add_argument("-s", "--station", help="Ground station ID", type=int)
+    parser.add_argument("CONSTRAINS_FILE", help="Input Priorities constrains file", type=str)
+    parser.add_argument("PRIORITIES_FILE", help="Output Priorities file", type=str)
+    args = parser.parse_args()
+
+    # Check arguments
+    if args.station is None:
+        parser.print_help()
+        sys.exit()
+
+    main(args.station, args.CONSTRAINS_FILE, args.PRIORITIES_FILE)
