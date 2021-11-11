2efce6fed5b5c12e6cb4bc1f7e5fc366d960ac77
Fabian P. Schmidt <kerel@mailbox.org>
2021-11-11 23:19:31 +0100

    test_cache: Refactor to be pytest-compatible
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- tests/test_cache.py
+++ tests/test_cache.py
@@ -1,21 +1,32 @@
 #!/usr/bin/env python3
 
 import logging
+from pathlib import Path
+from tempfile import gettempdir
 
-import settings
-from cache import CacheManager
-from satnogs_client import get_groundstation_info
+import pytest
+from auto_scheduler.cache import CacheManager
 
-if __name__ == '__main__':
-    logging.basicConfig(level=logging.INFO,
+CACHE_DIR = str(Path(gettempdir()))
+CACHE_AGE = 24
+MAX_NORAD_CAT_ID = 90000
+
+ground_station_id = 2
+ground_station_antenna = [{
+    'frequency': 430000000,
+    'frequency_max': 470000000,
+    'band': 'UHF',
+    'antenna_type': 'cross-yagi',
+    'antenna_type_name': 'Cross Yagi'
+}]
+
+
+@pytest.mark.skip(reason="this test takes approx. 2 minutes")
+def testCacheManager_force_update():
+    logging.basicConfig(level=logging.DEBUG,
                         format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
-
-    ground_station_id = 2
-    ground_station = get_groundstation_info(ground_station_id, allow_testing=True)
-    print(ground_station)
-
-    cache = CacheManager(ground_station_id, ground_station['antenna'], settings.CACHE_DIR,
-                         settings.CACHE_AGE, settings.MAX_NORAD_CAT_ID)
+    cache = CacheManager(ground_station_id, ground_station_antenna, CACHE_DIR, CACHE_AGE,
+                         MAX_NORAD_CAT_ID)
     print(cache.last_update())
     print(cache.update_needed())
 
