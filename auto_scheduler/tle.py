dbe33c51c0f904ec45febc999e926e9bbc89c18b
Fabian P. Schmidt <kerel@mailbox.org>
2023-02-06 22:10:43 +0100

    Fix parse_tle0 return value
    The existing call sites were expecting the name directly already.
    This was a bug which went undetected until a larger refactor / enabling
    of pylint message unearthed it by a type error:
    Some formatstring was expecting the name to be str, not dict.
    
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- auto_scheduler/tle.py
+++ auto_scheduler/tle.py
@@ -1,7 +1,11 @@
 def parse_tle0(tle0):
-    """Parse the tle0 line. This line only contains the
+    """
+    Parse the tle0 line. This line only contains the
     satellite name, but sometimes it is prefixed with '0 '.
+
+    Returns:
+    name of the satellite
     """
     if tle0[:2] == "0 ":
-        return {'name': tle0[2:].strip()}
-    return {'name': tle0.strip()}
+        return tle0[2:].strip()
+    return tle0.strip()
