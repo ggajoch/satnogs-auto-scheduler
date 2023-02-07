ggajoch/satnogs-auto-scheduler

commit e9d1c5f634b39bc88e61e6b4c43aeebe34b77a06

Use mostly fstrings
In some places the format-strings are too complicated to be replaced
with fstrings without extensive testing, thus this patch disables pylint
for those code blocks.

Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

auto_scheduler/cache.py
@@ -21,10 +21,10 @@ class CacheManager:
         self.max_norad_cat_id = max_norad_cat_id
 
         self.transmitters_file = os.path.join(self.cache_dir,
-                                              "transmitters_%d.txt" % self.ground_station_id)
-        self.tles_file = os.path.join(self.cache_dir, "tles_%d.json" % self.ground_station_id)
+                                              f"transmitters_{self.ground_station_id:d}.txt")
+        self.tles_file = os.path.join(self.cache_dir, f"tles_{self.ground_station_id:d}.json")
         self.last_update_file = os.path.join(self.cache_dir,
-                                             "last_update_%d.txt" % ground_station_id)
+                                             f"last_update_{ground_station_id:d}.txt")
 
         # Create cache
         if not os.path.isdir(self.cache_dir):
@@ -53,6 +53,8 @@ class CacheManager:
         return False
 
     def update(self, force=False):
+        # pylint: disable=consider-using-f-string
+
         if not force and not self.update_needed():
             # Cache is valid, skip the update
             return

auto_scheduler/satnogs_client.py
@@ -1,3 +1,5 @@
+# pylint: disable=consider-using-f-string
+
 import logging
 import sys
 from datetime import datetime

auto_scheduler/schedulers.py
@@ -24,6 +24,8 @@ def random_scheduler(passes, scheduledpasses, wait_time_seconds):
 
 
 def report_efficiency(scheduledpasses, passes):
+    # pylint: disable=consider-using-f-string
+
     if scheduledpasses:
         # Loop over passes
         start = False

schedule_single_station.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python3
+
+# pylint: disable=consider-using-f-string
+
 from __future__ import division
 
 import argparse

auto_scheduler/pass_predictor.py
@@ -72,7 +72,7 @@ def find_passes(satellite, observer, tmin, tmax, minimum_altitude, min_pass_dura
             if str(e).startswith("TLE elements are valid for a few weeks around their epoch"):
                 # pylint: disable=protected-access
                 age = observer.date.datetime() - sat_ephem._epoch.datetime()
-                print("ERROR: TLE too old: {}".format(age))
+                print(f"ERROR: TLE too old: {age}")
                 print(satellite.tle0.strip())
                 print(satellite.tle1.strip())
                 print(satellite.tle2.strip())

auto_scheduler/satellite.py
@@ -19,5 +19,6 @@ class Satellite:
         self.mode = mode
 
     def __repr__(self):
+        # pylint: disable=consider-using-f-string
         return "%s %s %d %d %d %s %s" % (self.id, self.transmitter, self.success_rate,
                                          self.good_count, self.data_count, self.mode, self.name)

auto_scheduler/utils.py
@@ -52,6 +52,7 @@ def satellites_from_transmitters(transmitters, tles):
 
 
 def print_scheduledpass_summary(scheduledpasses, ground_station_id, printer=print):
+    # pylint: disable=consider-using-f-string
     printer("GS   | Sch | NORAD | Start time          | End time            | Duration |  El | " +
             "Priority | Transmitter UUID       | Mode       | Satellite name ")
 

contrib/generate_priorities_file.py
@@ -20,7 +20,7 @@ class Preference:
         self.priority = priority
 
     def __repr__(self):
-        return "%s %f %f" % (self.search_term, self.success_rate, self.priority)
+        return f"{self.search_term:s} {self.success_rate:f} {self.priority:f}"
 
     def is_candidate(self, sat):
         # check if sat's name or transmitter is in our list
@@ -78,7 +78,7 @@ def main(station_id, constrains_file, priorities_file):
     # save the priorities in the file
     with open(priorities_file, "w") as out_file:
         for elem in sat_list.values():
-            out_file.writelines("{} {} {}\n".format(elem[0], elem[1], elem[2]))
+            out_file.writelines(f"{elem[0]} {elem[1]} {elem[2]}\n")
 
     logging.info(f'Done, output file written: {priorities_file}')
 

commit ae9429453a77eac0363b0474f55c6a1078ba655f

Fix logging format errors
Fixes a regression introduced by e9d1c5f6, ba8317cf and 0b764322.

The solution choosen in this patch is to rely on the string representation
thus alleviating the logging to be type-aware.

Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

auto_scheduler/cache.py
@@ -23,10 +23,9 @@ class CacheManager:
         self.max_norad_cat_id = max_norad_cat_id
 
         self.transmitters_file = os.path.join(self.cache_dir,
-                                              f"transmitters_{self.ground_station_id:d}.txt")
-        self.tles_file = os.path.join(self.cache_dir, f"tles_{self.ground_station_id:d}.json")
-        self.last_update_file = os.path.join(self.cache_dir,
-                                             f"last_update_{ground_station_id:d}.txt")
+                                              f"transmitters_{self.ground_station_id}.txt")
+        self.tles_file = os.path.join(self.cache_dir, f"tles_{self.ground_station_id}.json")
+        self.last_update_file = os.path.join(self.cache_dir, f"last_update_{ground_station_id}.txt")
 
         # Create cache
         if not os.path.isdir(self.cache_dir):

auto_scheduler/satnogs_client.py
@@ -86,10 +86,10 @@ def get_scheduled_passes_from_network(ground_station, tmin, tmax):
     client = requests.session()
 
     # Loop
-    next_url = f'{settings.NETWORK_BASE_URL}/api/observations/?ground_station={ground_station:d}'
+    next_url = f'{settings.NETWORK_BASE_URL}/api/observations/?ground_station={ground_station}'
     scheduledpasses = []
 
-    logger.info(f"Requesting scheduled passes for ground station {ground_station:d}")
+    logger.info(f"Requesting scheduled passes for ground station {ground_station}")
     # Fetch observations until the time of the end of the last fetched observation happends to be
     # before the start time of the selected timerange for scheduling
     # NOTE: This algorithm is based on the order in which the API returns the observations, i.e.
@@ -136,16 +136,16 @@ def get_scheduled_passes_from_network(ground_station, tmin, tmax):
             # Last fetched observation is older than the ROI for scheduling, end loop.
             break
 
-    logger.info(f"Scheduled passes for ground station {ground_station:d} retrieved!")
+    logger.info(f"Scheduled passes for ground station {ground_station} retrieved!")
     return scheduledpasses
 
 
 def get_groundstation_info(ground_station_id, allow_testing):
 
-    logger.info(f"Requesting information for ground station {ground_station_id:d}")
+    logger.info(f"Requesting information for ground station {ground_station_id}")
 
     # Loop
-    response = requests.get(f"{settings.NETWORK_BASE_URL}/api/stations/?id={ground_station_id:d}")
+    response = requests.get(f"{settings.NETWORK_BASE_URL}/api/stations/?id={ground_station_id}")
 
     selected_stations = list(filter(lambda s: s['id'] == ground_station_id, response.json()))
 
@@ -163,11 +163,11 @@ def get_groundstation_info(ground_station_id, allow_testing):
 
     if station['status'] == 'Testing' and not allow_testing:
         logger.info(
-            f"Ground station {ground_station_id:d} is in testing mode but auto-scheduling is not "
+            f"Ground station {ground_station_id} is in testing mode but auto-scheduling is not "
             "allowed. Use -T command line argument to enable scheduling.")
     else:
         logger.info(
-            f"Ground station {ground_station_id:d} neither in 'online' nor in 'testing' mode, "
+            f"Ground station {ground_station_id} neither in 'online' nor in 'testing' mode, "
             "can't schedule!")
     return {}
 
@@ -195,7 +195,7 @@ def schedule_observations_batch(observations):
             json=observations_serialized,
             headers={'Authorization': 'Token {}'.format(settings.SATNOGS_API_TOKEN)})
         response.raise_for_status()
-        logger.debug("Scheduled %d passes!", len(observations_serialized))
+        logger.debug("Scheduled {len(observations_serialized)} passes!")
     except requests.HTTPError:
         err = response.json()
         logger.error("Failed to batch-schedule the passes. Reason: %s", err)
@@ -223,4 +223,4 @@ def schedule_observations(observations_serialized):
             logger.debug("Scheduled pass!")
         except requests.HTTPError:
             err = response.json()
-            logger.error(f"Failed to schedule the pass at {observation['end']:s}. Reason: {err:s}")
+            logger.error(f"Failed to schedule the pass at {observation['end']}. Reason: {err}")

auto_scheduler/schedulers.py
@@ -43,7 +43,7 @@ def report_efficiency(scheduledpasses, passes):
         duration_total = tmax - tmin
 
         efficency = 100 * duration_scheduled.total_seconds() / duration_total.total_seconds()
-        logging.info(f"{len(scheduledpasses):d} passes selected out of {len(passes):d}, "
+        logging.info(f"{len(scheduledpasses)} passes selected out of {len(passes)}, "
                      f"{duration_scheduled.total_seconds():.0f} s out of "
                      f"{duration_total.total_seconds():.0f} s "
                      f"at {efficency:.3f}% efficiency")

schedule_single_station.py
@@ -258,7 +258,7 @@ def main():
 
     # Find passes
     passes = []
-    logging.info(f'Finding all passes for {len(satellites):d} satellites:')
+    logging.info(f'Finding all passes for {len(satellites)} satellites:')
 
     # Loop over satellites
     for satellite in tqdm(satellites, disable=None):
@@ -277,12 +277,12 @@ def main():
             logging.debug("Adjusted pass inside azimuth window is azr %f and azs %f",
                           float(satpass['azr']), float(satpass['azs']))
 
-            logging.debug(f"Original pass for {satellite.name:s}"
-                          f"is start {satpass['tr']:s} and end {satpass['ts']:s}")
+            logging.debug(f"Original pass for {satellite.name}"
+                          f"is start {satpass['tr']} and end {satpass['ts']}")
             satpass = constrain_pass_to_max_observation_duration(satpass, max_observation_duration,
                                                                  tmin, tmax)
-            logging.debug(f"Adjusted max observation duration for {satellite.name:s} "
-                          f"to start {satpass['tr']:s} and end {satpass['ts']:s}")
+            logging.debug(f"Adjusted max observation duration for {satellite.name} "
+                          f"to start {satpass['tr']} and end {satpass['ts']}")
 
             # pylint: disable=duplicate-code
             # NOTE: The transmitter was already added by find_passes initially.
@@ -310,8 +310,9 @@ def main():
 
     # List of scheduled passes
     scheduledpasses = get_scheduled_passes_from_network(ground_station_id, tmin, tmax)
-    logging.info(f"Found {len(scheduledpasses):d} scheduled passes "
-                 f"between {tmin:s} and {tmax:s} on ground station {ground_station_id:d}")
+
+    logging.info(f"Found {len(scheduledpasses)} scheduled passes "
+                 f"between {tmin} and {tmax} on ground station {ground_station_id}")
 
     # Get passes of priority objects
     prioritypasses, normalpasses = get_priority_passes(passes, priorities, favorite_transmitters,