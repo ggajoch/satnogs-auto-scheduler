7e62afdd1f5d76b79cecdc223d965dc62a3bd056
Fabian P. Schmidt <kerel@mailbox.org>
2023-02-17 22:50:56 +0100

    tests: Add unit test for utils.search_satellites method
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

---
+++ tests/test_utils.py
@@ -0,0 +1,43 @@
+import json
+
+from auto_scheduler.utils import search_satellites
+
+
+def key2int(dict_object):
+    """
+    Convert all keys to integer
+    """
+    return {int(key): value for key, value in dict_object.items()}
+
+
+def test_search_satellites():
+    """
+    Unit test for search_satellites using data for a typical UHF station.
+    """
+    # Load input data
+    with open('./tests/fixtures/transmitters_receivable.json') as fp_transmitters2:
+        transmitters_receivable = json.load(fp_transmitters2)
+
+    with open('./tests/fixtures/transmitters_stats.json') as fp_transmitters_stats:
+        transmitters_stats = json.load(fp_transmitters_stats)
+
+    with open('./tests/fixtures/tles.json') as fp_tles:
+        tles_all = json.load(fp_tles)
+
+    with open('./tests/fixtures/satellites.json') as fp_satellites:
+        satellites_by_norad_id_str = json.load(fp_satellites)
+    satellites_by_norad_id = key2int(satellites_by_norad_id_str)
+
+    max_norad_cat_id = 90000
+    skip_frequency_violators = True
+
+    # Load output data
+    with open('./tests/fixtures/search_satellites_output.json') as fp_output:
+        norad_cat_ids_fixture = set(json.load(fp_output))
+
+    satellites = search_satellites(transmitters_receivable, transmitters_stats, tles_all,
+                                   satellites_by_norad_id, max_norad_cat_id,
+                                   skip_frequency_violators)
+    norad_cat_ids = set(sat.id for sat in satellites)
+
+    assert norad_cat_ids == norad_cat_ids_fixture
