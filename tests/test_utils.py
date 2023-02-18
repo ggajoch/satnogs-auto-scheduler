1756edec7900bd0b7b9ebbc2b5646ce69059a2d7
Fabian P. Schmidt <kerel@mailbox.org>
2023-02-18 11:31:07 +0100

    tests: Check utils.search_satellites output for temporary satellites
    Signed-off-by: Fabian P. Schmidt <kerel@mailbox.org>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,5 +1,9 @@
 import json
+import tempfile
 
+import pytest
+
+from auto_scheduler.cache import CacheManager
 from auto_scheduler.utils import search_satellites
 
 
@@ -41,3 +45,33 @@ def test_search_satellites():
     norad_cat_ids = set(sat.id for sat in satellites)
 
     assert norad_cat_ids == norad_cat_ids_fixture
+
+
+@pytest.mark.skip(reason="takes approx. 4 minutes due to the transmitter statistics download")
+def test_search_satellites_online():
+    """
+    Check that search_satellites does not return satellites with temporary norad id (>90000):
+    """
+    ground_station_id = 1888
+    ground_station_antenna = [{
+        'antenna_type': 'yagi',
+        'antenna_type_name': 'Yagi',
+        'band': 'UHF',
+        'frequency': 430000000,
+        'frequency_max': 446000000
+    }]
+    cache_age = 24  # hours
+    max_norad_cat_id = 90000
+    skip_frequency_violators = True
+
+    with tempfile.TemporaryDirectory() as cache_dir:
+        cache = CacheManager(ground_station_id, ground_station_antenna, cache_dir, cache_age,
+                             max_norad_cat_id)
+        cache.update()
+
+    satellites = search_satellites(cache.transmitters_receivable, cache.transmitters_stats,
+                                   cache.tles_all, cache.satellites_by_norad_id, max_norad_cat_id,
+                                   skip_frequency_violators)
+
+    for satellite in satellites:
+        assert satellite.id < 90000
